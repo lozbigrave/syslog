f7dc591388dca3f0212c0a2c6c5d74712649bc0f
lozbigrave <42005842+lozbigrave@users.noreply.github.com>
2018-09-17 16:23:46 +0100

    Add files via upload

--- root/syslog_logrotate.sh
+++ root/syslog_logrotate.sh
@@ -1,60 +1,58 @@
 
 #!/bin/bash
 
-# Set the working target folders
-subfolders=$(find /syslogs -mindepth 3 -type d -regextype posix-extended -regex '.*[[:digit:]]{1,3}')
-
-# Set the retention in days
-retention=60
-
-# set the name of the logrotate log
-dailylog=logrotate.log
-
-#debug to a file
 set -x; exec 2>/root/debug_logrotate.log
-	 
-	 for i in $subfolders
+
+subfolders=$(find /syslogs -mindepth 3 -type d -regextype posix-extended -regex ".*[[:digit:]]{1,3}")
+retention=60
+masterlog=/root/logrotate.masterlog
+
+# Update the master log file
+echo "**This log is overwritten daily**" >$masterlog
+date >>$masterlog	 
+echo "Retention is $retention Days" >>$masterlog
+echo "Adding the following files into the archive:" >>$masterlog
+echo "---------------------------------------------" >>$masterlog
+
+# Begin the archive process: 
+
+	for i in $subfolders
 
           do
-#	 echo "changing into $i"
+
           cd $i
-                  if [ -f archive.tar.gz ]
+
+      	       	if [ -f archive.tar.gz ]
                   then
+		  echo "debug: archive exists, extracting"  
                   gzip -d archive.tar.gz
 
                   else
-                  true
+                  echo "debug: archive does not exist, nothing to extract"
+		  #true
 	  fi 
+		targetfiles=$(find $i -regextype posix-extended -regex ".*[[:digit:]]{1,3}"  -type f -mtime +$retention)	   
 	  
-		targetfiles=$(find $i -regextype posix-extended -regex '.*[[:digit:]]{1,3}'  -type f -mtime +$retention)	   
-	  
-	  
-	  		##### Per host directory logging overwritten daily ############################
-						
-			echo "" >$dailylog			
-		    date >>$dailylog
-			echo "**This log is overwritten daily." >>$dailylog
-            echo "" >$dailylog
-			echo "Retention is $retention Days" >>$dailylog
-			echo "Adding the following files into the archive" >>$dailylog
-            echo "---------------------------------------------" >>$dailylog
-            echo "$targetfiles" >>$dailylog
-            echo "---------------------------------------------" >>$dailylog
-            echo "**********************************************************" >>$dailylog
-                        
-			# Archive the files and remove them~
+			echo "$targetfiles" >>$masterlog
+			
+			echo "**This log is overwritten daily**" >logrotate.log
+			date >>logrotate.log	 
+			echo "Retention is $retention Days" >>logrotate.log
+			echo "Adding the following files into the archive:" >>logrotate.log
+			echo "---------------------------------------------" >>logrotate.log
+			echo "$targetfiles" >>logrotate.log
 
-			for x in $targetfiles; do tar -rvf archive.tar $x; done
-	
-			for y in $targetfiles; do 
-				echo "Deleting $y" >>$dailylog
-		       		rm $y
-				done
 
+				for x in $targetfiles
+				do tar -rvf archive.tar $x
+				rm $x	
+			    done
+			
              	gzip archive.tar
 	        
-		echo "" >>$dailylog
-	
-		chmod 770 $dailylog
-	
-	done	
+			echo "---------------------------------------------" >>logrotate.log
+                echo "Have a nice day :)" >>logrotate.log
+
+	done
+
+		chmod 770 $masterlog	
